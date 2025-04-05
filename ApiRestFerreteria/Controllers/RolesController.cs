4168263f67679734d6fa0422ae353ecf64c3ef05
catherinne21 <kathysantosreal@gmail.com>
2025-04-04 21:34:25 -0600

    Add files via upload
    Rolescontroller

--- ApiRestFerreteria/Controllers/RolesController.cs
+++ ApiRestFerreteria/Controllers/RolesController.cs
@@ -1,27 +1,45 @@
-﻿using ApiRestFerreteria.Models;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using ApiRestFerreteria.Rol;
 
 namespace ApiRestFerreteria.Controllers
 {
     public class RolesController : ApiController
     {
+        public RolesController() : base() { }
 
-        private readonly Data.RolesRepository _repo = new Data.RolesRepository();
-
-        // GET: api/Roles
-        [HttpGet]
-        [Route("api/roles")]
-        public IHttpActionResult GetRoles()
+        [HttpPost]
+        [Route("rest/api/insertarRol")]
+        public IHttpActionResult insertarRol(csEstructuraRol.requestRol model)
         {
-            var roles = _repo.GetRoles();
+            return Ok(new csRol().insertarRol(model.Nombre, model.Sueldo));
+        }
 
-            if (roles == null || !roles.Any())
-            {
-                return NotFound();
-            }
+        [HttpGet]
+        [Route("rest/api/obtenerRoles")]
+        public IHttpActionResult obtenerRoles()
+        {
+            return Ok(new csRol().obtenerRoles());
+        }
 
-            return Ok(roles);
+        [HttpGet]
+        [Route("rest/api/obtenerRol/{id}")]
+        public IHttpActionResult obtenerRol(int id)
+        {
+            return Ok(new csRol().obtenerRolPorId(id));
+        }
+
+        [HttpPut]
+        [Route("rest/api/actualizarRol/{id}")]
+        public IHttpActionResult actualizarRol(int id, csEstructuraRol.requestRol model)
+        {
+            return Ok(new csRol().actualizarRol(id, model.Nombre, model.Sueldo));
+        }
+
+        [HttpDelete]
+        [Route("rest/api/eliminarRol/{id}")]
+        public IHttpActionResult eliminarRol(int id)
+        {
+            return Ok(new csRol().eliminarRol(id));
         }
     }
 }
