2ef8735dae704b2e6bb47e7cfb8a865e433bf0c0
2022-049655@intecap.edu.gt <2022-049655@intecap.edu.gt>
2025-04-18 04:36:24 -0600

    Cambios articulos

---
+++ ApiRestFerreteria/Controllers/ArticulosController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using ApiRestFerreteria.Articulo;
+
+namespace ApiRestFerreteria.Controllers
+{
+    public class ArticulosController : ApiController
+    {
+        private csArticulo articuloService = new csArticulo();
+
+        [HttpPost]
+        [Route("rest/api/crearArticulo")]
+        public IHttpActionResult crearArticulo([FromBody] csEstructuraArticulo.requestArticulo model)
+        {
+            return Ok(articuloService.insertarArticulo(model.NombreArticulo, model.Precio, model.Stock, model.IdCategoria, model.IdProveedor));
+        }
+
+        [HttpGet]
+        [Route("rest/api/obtenerArticulos")]
+        public IHttpActionResult obtenerArticulos()
+        {
+            return Ok(articuloService.obtenerArticulos());
+        }
+
+        [HttpPut]
+        [Route("rest/api/actualizarArticulo/{id}")]
+        public IHttpActionResult actualizarArticulo(int id, [FromBody] csEstructuraArticulo.requestArticulo model)
+        {
+            return Ok(articuloService.actualizarArticulo(id, model.NombreArticulo, model.Precio, model.Stock));
+        }
+
+        [HttpDelete]
+        [Route("rest/api/eliminarArticulo/{id}")]
+        public IHttpActionResult eliminarArticulo(int id)
+        {
+            return Ok(articuloService.eliminarArticulo(id));
+        }
+    }
+}
---
+++ ApiRestFerreteria/Models/csEstructuraArticulo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace ApiRestFerreteria.Articulo
+{
+    public class csEstructuraArticulo
+    {
+        public class requestArticulo
+        {
+            public string NombreArticulo { get; set; }
+            public decimal Precio { get; set; }
+            public int Stock { get; set; }
+            public int IdCategoria { get; set; }
+            public int IdProveedor { get; set; }
+        }
+
+        public class responseArticulo
+        {
+            public int respuesta { get; set; }
+            public string descripcion_respuesta { get; set; }
+        }
+
+        public class articulo
+        {
+            public int IdArticulo { get; set; }
+            public string NombreArticulo { get; set; }
+            public decimal Precio { get; set; }
+            public int Stock { get; set; }
+        }
+
+        public class responseAllArticulos
+        {
+            public List<articulo> articulos { get; set; }
+            public DataTable listaArticulos { get; set; }
+        }
+    }
+}
